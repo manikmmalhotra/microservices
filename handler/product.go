3c8cd7c7c23294f24bc6a09d646d62a6d465bff8
manikmmalhotra <manikmalhotra2113@gmail.com>
2021-10-05 20:39:20 +0530

    first commit

---
+++ handler/product.go
@@ -0,0 +1,142 @@
+// Package classification of Product API.
+//
+// Documentation for Product API
+//
+//	Schemes: http
+//	BasePath: /
+//	Version: 1.0.0
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
+
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"gp/data"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func Newproducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+// func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		p.getProducts(rw, r)
+// 		return
+// 	}
+
+// 	if r.Method == http.MethodPut {
+// 		rx := regexp.MustCompile(`/([0-9]+)`)
+// 		g := rx.FindAllStringSubmatch(r.URL.Path, -1)
+// 		if len(g) != 1 {
+// 			http.Error(rw, "Inavalid URI", http.StatusBadRequest)
+// 			return
+// 		}
+// 		if len(g[0]) != 2 {
+// 			http.Error(rw, "Inavalid URI", http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		idString := g[0][1]
+// 		id, err := strconv.Atoi(idString)
+// 		if err != nil {
+// 			http.Error(rw, "Bad URI", http.StatusInternalServerError)
+// 		}
+// 		fmt.Println(id)
+// 		p.updateProducts(id, rw, r)
+// 		return
+
+// 	}
+// 	if r.Method == http.MethodPost {
+// 		p.addProducts(rw, r)
+// 		return
+// 	}
+// 	rw.WriteHeader((http.StatusMethodNotAllowed))
+// }
+
+func (p *Products) GetProducts(rw http.ResponseWriter, h *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "unable to marshall json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) AddProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
+}
+
+func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Println("Handle PUT Product", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := data.Product{}
+
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+		// validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Error validating product: %s", err),
+				http.StatusBadRequest,
+			)
+			return
+		}
+
+		// add the product to the context
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
+
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(rw, r)
+	})
+}
